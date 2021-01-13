d60fab77cdb15f47bceedb4441fbe26d6e68d197
Aleks Mitskevich <ADMitskevich@sberbank.ru>
2021-01-13 21:18:46 +0300

    add source to level4

---
+++ level4/source.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int m;
+
+void p(char *s)
+{
+    printf(s);
+}
+
+void n()
+{
+    char s[520];
+
+    fgets(s, 512, stdin);
+    p(s);
+    if (m == 16930116) {
+        system("/bin/cat /home/user/level5/.pass");
+    }
+}
+
+int main()
+{
+    n();
+    return 0;
+}
